92c2cb54462b295899dc0b85ac282a49e5b7e9a0
Daviwizzy <148009182+Daviwizzy@users.noreply.github.com>
2024-04-21 00:20:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['2349035732952', 'Daviwhizzy', true],
+  ['2349044974094', 'David', true], 
   [''] 
 ] //Number of owners
 
